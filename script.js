97b5cf9b83d14afa990036f5fa77474f1ee875f6
Saad Kulumba <75015106+skulumba@users.noreply.github.com>
2024-11-09 01:45:55 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple interactive functions can be added if needed
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to my personal cybersecurity portfolio site!');
+});
